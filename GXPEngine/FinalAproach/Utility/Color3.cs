52812de020be6ca03c6be30fcd08cb3df94dafac
Matioma <Artiom.Matievschi@gmail.com>
2020-05-06 12:45:14 +0200

    Add color struct

---
+++ GXPEngine/FinalAproach/Utility/Color3.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+
+public struct Color3
+{
+    public int x;
+    public int y;
+    public int z;
+    public Color3(int x = 0,int y = 0,int z=0) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
+
