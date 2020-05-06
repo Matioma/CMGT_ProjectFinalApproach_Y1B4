5492ec97348468ca7ae0403a8ec41735315bac8d
Matioma <Artiom.Matievschi@gmail.com>
2020-05-06 11:04:23 +0200

    Add Cursor customizing

---
+++ GXPEngine/FinalAproach/Controller.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GXPEngine;
+
+
+public class Controller:GameObject
+{
+    readonly Cursor cursor;
+
+
+        
+    public Controller()
+    {
+        cursor = new Cursor(60,60);
+        AddChild(cursor);
+    }
+
+    void Update() {
+        cursor.SetXY(Input.mouseX, Input.mouseY);
+    
+    }
+}
---
+++ GXPEngine/FinalAproach/Cursor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using GXPEngine;
+
+class Cursor:AnimationSprite
+{
+    public Cursor():base("Art/brush.png", 2, 1) {   
+    }
+
+    public Cursor(int width, int height) : this() {
+        this.width = width;
+        this.height = height;
+    }
+
+}
